43ba380a156841caf31519de59dc2c8c5c86c3f8
Nguyễn Khánh Đan <96256583+NguyenKhanhDan@users.noreply.github.com>
2023-09-23 18:54:12 +0700

    update entity follower

--- be-destiny-community/BE_Destiny/src/main/java/com/davisy/entity/Follower.java
+++ be-destiny-community/BE_Destiny/src/main/java/com/davisy/entity/Follower.java
@@ -1,5 +1,9 @@
 package com.davisy.entity;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,22 +11,37 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@Table(name = "follower")
+@Table(name = "follower", uniqueConstraints = { @UniqueConstraint(columnNames = { "follower_id", "user_id" }) })
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Follower {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	int follower_id;
+public class Follower{
+	@EmbeddedId
+	private Pk pk;
 	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	User user;
+	@Embeddable
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Pk implements Serializable{
+		private static final long serialVersionUID = 1L;
+		int follower_id;
+		int user_id;
+		
+		@Override
+		public String toString() {
+			return "Pk [follower_id = " + follower_id + ", user_id = " + user_id + "]";
+		}
+	}
 	
+	@Override
+	public String toString() {
+	    return "Employee [pk=" + pk + "]";
+	  }
 }
