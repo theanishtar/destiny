ac526f38b41a87e04e29bdcc92b00beb48329c94
theanishtar <dangtt135@gmail.com>
2023-12-18 17:57:04 +0700

    fix DAO

--- be-destiny-community/BE_Destiny/src/main/java/com/davisy/dao/ProvinceDAO.java
+++ be-destiny-community/BE_Destiny/src/main/java/com/davisy/dao/ProvinceDAO.java
@@ -2,12 +2,13 @@ package com.davisy.dao;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.davisy.entity.Provinces;
-
-public interface ProvinceDao extends JpaRepository<Provinces, Integer>{
+//@Cacheable("provinces")//Tạo bộ nhớ đệm
+public interface ProvinceDAO extends JpaRepository<Provinces, Integer>{
 
 	//22-9-2023 -tìm thành phố/tỉnh theo id
 	@Query(value = "SELECT * FROM provinces WHERE code=:code", nativeQuery = true)
@@ -19,6 +20,10 @@ public interface ProvinceDao extends JpaRepository<Provinces, Integer>{
 	
 	//11-10-2023 -tìm mã tỉnh theo tên
 	@Query(value = "SELECT code FROM provinces WHERE full_name=:provinceName", nativeQuery = true)
-	public String provinceCode(String provinceName);	
+	public String provinceCode(String provinceName);
+	
+	@Query(value = "SELECT * FROM provinces WHERE full_name=:provinceName", nativeQuery = true)
+	public Provinces provinces(String provinceName);
+	
 	
 }
